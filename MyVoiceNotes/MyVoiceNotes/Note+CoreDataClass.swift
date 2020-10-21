7011731df7155565f3c045e8130879f95b0eef4a
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-10-20 22:24:05 -0300

    Configure model from core data

---
+++ MyVoiceNotes/MyVoiceNotes/Note+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Note+CoreDataClass.swift
+//  MyVoiceNotes
+//
+//  Created by Ronaldo Gomes on 20/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+}
